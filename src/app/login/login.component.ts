cd808863347fc9717782c42b518f7d85efdc40b5
daftclaud <cdsmileyone@gmail.com>
2019-02-25 23:28:58 -0500

    authGuard fix with observable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,71 +6,75 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { AuthService } from '../services/auth.service';
 
 @Component({
-  	selector: 'app-login',
-  	templateUrl: './login.component.html',
-  	styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent implements OnInit {
+  constructor(
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor(private snackBar: MatSnackBar, public dialog: MatDialog, private authService: AuthService, private router: Router) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   forgotPassword(): void {
-    const dialogRef = this.dialog.open(ForgotPasswordDialog, {width: '300px', data: {}});
-	  dialogRef.afterClosed().subscribe(result => {console.log('The dialog was closed');});
+    const dialogRef = this.dialog.open(ForgotPasswordDialog, {
+      width: '300px',
+      data: {}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
-	login(): void {
-		if (this.loginForm.controls.userType.value == '') {
-			this.snackBar.open('Please select either Band or Venue above.', 'Close', {duration: 3000,});
-		}
+  login(): void {
 
-		if (this.loginForm.valid) {
-			this.snackBar.dismiss();
-      this.authService.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value).then((res) => {
-        this.router.navigate(['browse']);
+    if (this.loginForm.controls.userType.value === '') {
+      this.snackBar.open('Please select either Band or Venue above.', 'Close', {
+        duration: 3000
       });
     }
-	}
+
+
+    if (this.loginForm.valid) {
+      this.snackBar.dismiss();
+      this.authService
+        .login(
+          this.loginForm.controls.email.value,
+          this.loginForm.controls.password.value
+        )
+        .then(res => {
+          this.router.navigate(['browse']);
+        });
+    }
+  }
 
   status(): void {
     console.log(this.authService.authenticated);
   }
 
-	loginForm: FormGroup = new FormGroup({
-  	email: new FormControl('', [
-  		Validators.required,
-  		Validators.email,
-  	]),
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
 
-  	password: new FormControl('', [
-  		Validators.required,
-  	]),
+    password: new FormControl('', [Validators.required]),
 
-  	userType: new FormControl('', [
-  		Validators.required,
-  	])
-	});
+    userType: new FormControl('', [Validators.required])
+  });
 }
 
 @Component({
   selector: 'forgot-password-dialog',
-  templateUrl: 'forgot-password-dialog.html',
+  templateUrl: 'forgot-password-dialog.html'
 })
-
 export class ForgotPasswordDialog {
-
   constructor(public dialogRef: MatDialogRef<ForgotPasswordDialog>) {}
 
-	forgotPasswordForm: FormGroup = new FormGroup({
-  	email: new FormControl('', [
-  		Validators.required,
-  		Validators.email,
-  	]),
-	});
+  forgotPasswordForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
   send(): void {
     this.dialogRef.close();
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
 
-	constructor(private authService: AuthService, private router: Router) { }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
 
-	canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-		if (this.authService.authenticated) {
-			return true;
-		} else {
-			this.router.navigate(['/login']);
-			return false;
-		}
-	}
+    return this.authService.currentUserObservable.pipe(
+      map(user => {
+        return user ? true : false;
+      }),
+      take(1)
+    );
+  }
 }

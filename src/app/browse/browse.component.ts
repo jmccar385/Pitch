a4ca4d9633cb81156b203de348614385755ca9a8
Camden Marchetti <camden.marchetti@gmail.com>
2019-03-04 15:20:30 -0500

    Shell of browse screen tiles

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar, MatDialog } from '@angular/material';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 
@@ -9,10 +10,40 @@ import { Router } from '@angular/router';
 })
 export class BrowseComponent implements OnInit {
 
-	constructor(private authService: AuthService, private router: Router) { }
+	constructor(private snackBar: MatSnackBar, public dialog: MatDialog, private authService: AuthService, private router: Router) { }
+
+	private profileCards: any[] = [
+		{
+			profile_image: 'https://www.shareicon.net/data/128x128/2017/05/24/886426_user_512x512.png',
+			profile_name: 'Test profile',
+			rating: [
+				{source: 'https://www.shareicon.net/data/128x128/2016/01/03/697542_star_512x512.png'},
+				{source: 'https://www.shareicon.net/data/128x128/2016/01/03/697542_star_512x512.png'},
+				{source: 'https://www.shareicon.net/data/128x128/2016/01/03/697542_star_512x512.png'},
+				{source: 'https://www.shareicon.net/data/128x128/2015/09/21/644056_star_512x512.png'},
+				{source: 'https://www.shareicon.net/data/128x128/2016/01/05/698469_star_512x512.png'}
+			],
+			upcoming_event_text: '3 upcoming shows',
+
+			amplifiers_available: true,
+			drums_available: true,
+			guitars_available: true,
+			microphones_available: true,
+
+			amp_image: 'https://www.shareicon.net/data/128x128/2016/07/06/111207_box_512x512.png',
+			drum_image: 'https://www.shareicon.net/data/128x128/2015/10/21/659440_music_512x512.png',
+			guitar_image: 'https://www.shareicon.net/data/64x64/2015/10/20/659234_music_512x512.png',
+			mic_image: 'https://www.shareicon.net/data/64x64/2015/10/19/658679_music_512x512.png'
+		}
+	];
 
   	ngOnInit() {
   	}
+
+	getCards(start, end): void {
+		// TODO: Call API to get profiles in range 
+		// [start .. end] using user filter data
+	}
 
 	logout(): void {
 		this.authService.logout();

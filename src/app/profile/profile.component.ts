45e1aa7b2d64587267ece9cb3827cae63960ed2c
Camden Marchetti <camden.marchetti@gmail.com>
2019-03-11 18:27:41 -0400

    Rebased to master

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,7 +13,15 @@ import { ReviewDialog } from "./review.component"
 })
 export class ProfileComponent implements OnInit {
   private uid: string;
+<<<<<<< HEAD
   private profile: any = null;
+=======
+  private profile_data: any = null;
+  profileUrl: string;
+  editAbout: boolean = false;
+  editAddress: boolean = false;
+  slideIndex: number = 1;
+>>>>>>> Added web-animations-js and image slideshow on profile
 
   constructor(
     public dialog: MatDialog,
@@ -26,6 +34,7 @@ export class ProfileComponent implements OnInit {
     this._setData(this.route.snapshot.params["id"], this.route.snapshot.params["userType"]);
   }
 
+<<<<<<< HEAD
   private _setData(uid: string, userType: string) {
     if (userType == "band") {
       this.profileService
@@ -46,6 +55,48 @@ export class ProfileComponent implements OnInit {
     }
   }
 
+=======
+  private _getCurrentUserProfile() {
+    if (!this.authService.currentUserID) {
+      this.authService.currentUserObservable.subscribe(user => this._setData(user.uid));
+    } else {
+      this._setData(this.authService.currentUserID);
+    }
+  }
+
+  private _setData(uid: string) {
+    this.profileService
+      .getArtistObserverById(uid)
+      .then(doc => (doc.exists ? (this.profile_data = [doc.data()]) : [null]));
+  }
+
+  private changeSlideBy(delta) {
+    this.showSlides(this.slideIndex += delta);
+  }
+
+  private jumpToSlide(target) {
+    this.showSlides(this.slideIndex = target + 1);
+  }
+
+  private showSlides(index) {
+    let slides = document.getElementsByClassName("slide-image");
+    let dots = document.getElementsByClassName("slide-dot");
+
+    if (slides.length == 0 || dots.length == 0) return;
+
+    if (index > slides.length) this.slideIndex = 1;
+    if (index < 1) this.slideIndex = slides.length;
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].className = slides[i].className.replace(' slide-active', '');
+      dots[i].className = dots[i].className.replace(' dot-active', '');
+    }
+
+    slides[this.slideIndex - 1].className += " slide-active";
+    dots[this.slideIndex - 1].className += " dot-active";
+  }
+
+>>>>>>> Added web-animations-js and image slideshow on profile
   reviewModal(): void {
     const dialogRef = this.dialog.open(ReviewDialog, {width: "300px", data: {}});
   }

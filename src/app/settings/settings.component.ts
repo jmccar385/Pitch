199974d8fda401b54613da7bbaa1dd59905b4644
Camden Marchetti <camden.marchetti@gmail.com>
2019-05-07 18:55:58 -0400

    Broke out components

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
-import { MatSnackBar, MatDialog } from '@angular/material';
-import { NewEmailDialogComponent } from './newemail.component';
 
 @Component({
   selector: 'app-settings',
@@ -12,64 +10,12 @@ import { NewEmailDialogComponent } from './newemail.component';
 export class SettingsComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userType = this.authService.userType;
+  }
 
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
-}
-
-@Component({
-  selector: 'app-account-settings',
-  templateUrl: './account.settings.component.html',
-  styleUrls: ['./settings.component.css']
-})
-export class AccountSettingsComponent implements OnInit {
-  constructor(
-    private authService: AuthService,
-    public dialog: MatDialog,
-    private snackBar: MatSnackBar
-  ) {}
-
-  userType: string;
-  verified = false;
-  userEmail = '';
-
-  ngOnInit() {
-    this.userType = this.authService.userType;
-    this.verified = this.authService.currentUser.emailVerified;
-    this.userEmail = this.authService.currentUser.email;
-  }
-
-  delete(): void {
-    // Todo: delete account
-  }
-
-  verifyEmail() {
-    this.authService.verification().then(() => {
-      this.snackBar.open('A new email has been sent.', 'close', {
-        duration: 2000
-      });
-    });
-  }
-
-  changeEmail() {
-    this.dialog.open(NewEmailDialogComponent, {
-      width: '450px',
-      data: {}
-    });
-  }
-}
-
-
-@Component({
-  selector: 'app-account-settings',
-  templateUrl: './notifications.settings.component.html',
-  styleUrls: ['./settings.component.css']
-})
-export class NotificationSettingsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
 }
